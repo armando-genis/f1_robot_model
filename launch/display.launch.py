acc20c525237aad8f7164977cc9435c8dc8cb9a3
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-08-12 16:29:28 -0600

    Lidar hokuyo addded

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -18,16 +18,7 @@ def generate_launch_description():
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
-
-
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
@@ -38,19 +29,17 @@ def generate_launch_description():
     spawn_entity = launch_ros.actions.Node(
     package='gazebo_ros',
     executable='spawn_entity.py',
-    arguments=['-entity', 'truck', '-topic', 'robot_description'],
+    arguments=['-entity', 'racecar', '-topic', 'robot_description'],
     output='screen'
     )
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
+
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
         launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
         spawn_entity,
         rviz_node
