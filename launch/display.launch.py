dcfd27e47eef1a9dee4a302492d905636beb5937
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-08-13 19:43:51 -0600

    use_sim_time added
    This option make the simulation run with at the same frame rate as gazebo and rviz. We set the use_sim_time added as true.

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -2,22 +2,33 @@ import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='f1_robot_model').find('f1_robot_model')
     default_model_path = os.path.join(pkg_share, 'urdf/racecar.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     world_path=os.path.join(pkg_share, 'world/my_world.sdf')
+    use_sim_time = LaunchConfiguration('use_sim_time') 
+    
+    
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+        )
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'use_sim_time': use_sim_time, 'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
+    
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time}],
     )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -25,6 +36,7 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
+        parameters=[{'use_sim_time': use_sim_time}],
     )
     spawn_entity = launch_ros.actions.Node(
     package='gazebo_ros',
@@ -47,6 +59,7 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
         launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
+        declare_use_sim_time_cmd,
         joint_state_publisher_node,
         robot_state_publisher_node,
         spawn_entity,
